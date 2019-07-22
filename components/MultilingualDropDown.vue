2db32d1cb5d26cf8f63e366a684c750ab3469779
Keerthi26 <keerthi2592@gmail.com>
2019-07-22 11:58:28 +0530

    Update value while typing

---
+++ components/MultilingualDropDown.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.multilingual-dropdown
+  //- .mask(v-if="open", @click="hide")
+  //- a.selected(@click="toggle")
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+// @import 'assets/styles/includes'
+
+</style>
---
+++ components/MultilingualTextBox.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.text-box
+  span.input(v-for="v, lang in value", :key="lang")
+    input(type="text", :name="lang", :value="v", @input="update")
+    span {{ lang }}
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  methods: {
+    update(e) {
+      const { name, value } = e.target
+      const newValue = { ...this.value, [name]: value }
+      this.$emit('change', newValue)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.text-box
+  margin: 1rem 2rem
+  .input
+    display: flex
+  input
+    color: #000
+    ::placholder
+      color: #ccc
+</style>
